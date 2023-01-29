d7fb1c504e1c2dff400608beb49507395e2cabd5
Yaroslav <yaroslav.usp@gmail.com>
2023-01-29 21:27:00 +0300

    Simple movement in random direction

--- layout.py
+++ layout.py
@@ -1,6 +1,8 @@
 from random import randint
 from typing import List
-# import pygame
+
+
+import pygame
 
 
 class Grid:
@@ -14,28 +16,87 @@ class Grid:
     def fill_with_food(self):
         for y in range(self.HEIGHT):
             for x in range(self.WIDTH):
-                if randint(0, 5) == 0:
+                if randint(0, 3) == 0:
                     self.grid[y].append(1)
                 else:
                     self.grid[y].append(0)
 
     def add_cell(self, position: tuple):
-        cell = Cell((position[0], position[1]))
+        cell = Cell((position[0], position[1]), self.grid)
         self.grid[position[0]][position[1]] = 2
         return cell
 
 
 class Cell:
-    def __init__(self, initial_position: tuple):
+    def __init__(self, initial_position: tuple, grid: List[List]):
         self.CELL_COLOR = (72, 61, 139)
         self.position = initial_position
-        self.food_nearby = []
+        self.nearest_food = []
+        self.grid = grid
+        self.energy = 100
+        # self.alive = True
 
-    def find_food(self, grid: List[List]):
-        for y_local in range(self.position[0]-2, self.position[0]+3):
-            for x_local in range(self.position[1]-2, self.position[1]+3):
+    def find_food(self):
+        food_nearby = []
+        for y_local in range(self.position[0] - 2, self.position[0] + 3):
+            for x_local in range(self.position[1] - 2, self.position[1] + 3):
                 try:
-                    if grid[y_local][x_local] == 1:
-                        self.food_nearby.append((y_local, x_local))
+                    if self.grid[y_local][x_local] == 1:
+                        food_nearby.append((y_local, x_local))
                 except IndexError as e:
                     return y_local, x_local, e
+
+        distances = {}
+        for food in food_nearby:
+            if abs(self.position[0] - food[0]) == 2 or abs(self.position[1] - food[1]) == 2:
+                distances[food] = 2
+            else:
+                distances[food] = 1
+
+        for xy in distances.keys():
+            if distances[xy] == min(distances.values()):
+                self.nearest_food.append(xy)
+
+    def move(self):
+        if self.energy <= 0:
+            return
+        n = randint(0, 3)
+        # Проблема: когда клетка доходит до границы и выходит за нее сверху, она перемещается вниз!
+
+        if n == 0:
+            lst = list(self.position)
+            self.grid[lst[0]][lst[1]] = 0
+            lst[0] = lst[0] - 1
+            self.energy -= 1
+            self.position = tuple(lst)
+            if self.grid[self.position[0]][self.position[1]] == 1:
+                self.energy += 10
+            self.grid[lst[0]][lst[1]] = 2
+        elif n == 1:
+            lst = list(self.position)
+            self.grid[lst[0]][lst[1]] = 0
+            lst[0] = lst[0] + 1
+            self.energy -= 1
+            self.position = tuple(lst)
+            if self.grid[self.position[0]][self.position[1]] == 1:
+                self.energy += 10
+            self.grid[lst[0]][lst[1]] = 2
+        elif n == 2:
+            lst = list(self.position)
+            self.grid[lst[0]][lst[1]] = 0
+            lst[1] = lst[1] - 1
+            self.energy -= 1
+            self.position = tuple(lst)
+            if self.grid[self.position[0]][self.position[1]] == 1:
+                self.energy += 10
+            self.grid[lst[0]][lst[1]] = 2
+        elif n == 3:
+            lst = list(self.position)
+            self.grid[lst[0]][lst[1]] = 0
+            lst[1] = lst[1] + 1
+            self.energy -= 1
+            self.position = tuple(lst)
+            if self.grid[self.position[0]][self.position[1]] == 1:
+                self.energy += 10
+            self.grid[lst[0]][lst[1]] = 2
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Main:
         pygame.init()
         self.window = pygame.display.set_mode((800, 400))
         pygame.display.set_caption("Evolution")
-        self.FPS = 2
+        self.FPS = 10
         self.clock = pygame.time.Clock()
 
     def run(self):
@@ -16,20 +16,24 @@ class Main:
         # grid[строка][столбец] !!!
         grid.fill_with_food()
         # у add_cell сначала строка, затем столбец !!!
-        cell = grid.add_cell((25, 50))
-        cell.find_food(grid.grid)
-        print(cell.food_nearby)
+        cell = grid.add_cell((25, 65))
+        cell2 = grid.add_cell((25, 35))
+        # cell.find_food()
 
         while True:
             self.clock.tick(self.FPS)
             self.window.fill((255, 255, 255))
+
+            cell.move()
+            cell2.move()
+
             for y in range(grid.HEIGHT):
                 for x in range(grid.WIDTH):
                     if grid.grid[y][x] == 1:
                         # у draw сначала столбец потом строка !!!
-                        pygame.draw.rect(self.window, grid.FOOD_COLOR, (8 * x, 8 * y, 8, 8))
+                        pygame.draw.rect(self.window, grid.FOOD_COLOR, (8*x + 2, 8*y + 2, 4, 4))
                     elif grid.grid[y][x] == 2:
-                        pygame.draw.rect(self.window, cell.CELL_COLOR, (8 * x, 8 * y, 8, 8))
+                        pygame.draw.rect(self.window, cell.CELL_COLOR, (8*x, 8*y, 8, 8))
             pygame.display.update()
 
             for event in pygame.event.get():
