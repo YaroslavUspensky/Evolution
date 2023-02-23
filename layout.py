9ed2e01e11af0a1e2460d1cae1dca272246a72c1
Yaroslav <yaroslav.usp@gmail.com>
2023-02-23 11:44:46 +0300

    working version

--- layout.py
+++ layout.py
@@ -1,1 +1,92 @@
+from random import randint
+import pygame
 
+all_sprites = pygame.sprite.Group()
+cell_sprites = pygame.sprite.Group()
+
+WIDTH = 800
+HEIGHT = 400
+FPS = 10
+
+
+class Cell(pygame.sprite.Sprite):
+    last_move = 0
+
+    def __init__(self, initial_x, initial_y, initial_resistance):
+        super().__init__()
+        self.image = pygame.image.load("Cell.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = initial_x
+        self.rect.y = initial_y
+        self.lifetime = 0
+        self.max_lifetime = 100
+
+        self.resistance = initial_resistance
+        self.concentration = 0
+
+    def change_zone(self):
+        if 0 < self.rect.x < WIDTH/4:
+            self.concentration = 0
+        elif WIDTH/4 < self.rect.x < WIDTH/2:
+            self.concentration = 15
+            self.max_lifetime = 85
+        elif WIDTH/2 < self.rect.x < 3*WIDTH/4:
+            self.concentration = 50
+        elif 3*WIDTH/4 < self.rect.x < WIDTH:
+            self.concentration = 80
+
+    def mutate(self):
+        if randint(0, 100) == 1:
+            self.resistance += 10
+            self.image.fill((100, 100, 100))
+
+    def duplicate(self):
+        child = Cell(self.rect.x + 5, self.rect.y, self.resistance)
+        all_sprites.add(child)
+        cell_sprites.add(child)
+
+    def move(self):
+        n = randint(0, 7)
+        if n > 4:
+            n = self.last_move
+
+        if n == 0:
+            self.rect.x += 4
+        elif n == 1:
+            self.rect.x -= 4
+        elif n == 2:
+            self.rect.y += 4
+        elif n == 3:
+            self.rect.y -= 4
+        self.last_move = n
+
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = WIDTH
+        if self.rect.top < 0:
+            self.rect.bottom = HEIGHT
+        if self.rect.bottom > HEIGHT:
+            self.rect.top = 0
+
+    def update(self):
+        self.lifetime += 1
+        self.mutate()
+        self.change_zone()
+
+        if self.resistance < self.concentration:
+            self.max_lifetime = 100 - self.concentration
+
+        if self.lifetime >= self.max_lifetime:
+            self.kill()
+            return
+        if self.lifetime == 20:
+            self.duplicate()
+
+        self.move()
+
+
+class Info(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
--- main.py
+++ main.py
@@ -1,67 +1,5 @@
 import sys
-
-import pygame
-from random import randint
-
-
-WIDTH = 800
-HEIGHT = 400
-GRID_WIDTH = 100
-GRID_HEIGHT = 50
-AMOUNT_OF_FOOD = 5
-FPS = 10
-
-all_sprites = pygame.sprite.Group()
-cell_sprites = pygame.sprite.Group()
-
-
-class Cell(pygame.sprite.Sprite):
-    last_move = 0
-
-    def __init__(self, initial_x, initial_y):
-        super().__init__()
-        self.image = pygame.image.load("Cell.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = initial_x
-        self.rect.y = initial_y
-        self.lifetime = 0
-        self.max_lifetime = 10*FPS
-
-    def update(self):
-        self.lifetime += 1
-        if self.lifetime >= self.max_lifetime:
-            self.kill()
-
-        if self.lifetime == 30:
-            self.division()
-
-        n = randint(0, 7)
-        if n > 4:
-            n = self.last_move
-
-        if n == 0:
-            self.rect.x += 4
-        elif n == 1:
-            self.rect.x -= 4
-        elif n == 2:
-            self.rect.y += 4
-        elif n == 3:
-            self.rect.y -= 4
-        self.last_move = n
-
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
-        if self.rect.right < 0:
-            self.rect.left = WIDTH
-        if self.rect.top < 0:
-            self.rect.bottom = HEIGHT
-        if self.rect.bottom > HEIGHT:
-            self.rect.top = 0
-
-    def division(self):
-        child = Cell(self.rect.x + 5, self.rect.y)
-        all_sprites.add(child)
-        cell_sprites.add(child)
+from layout import *
 
 
 class Main:
@@ -71,25 +9,46 @@ class Main:
         pygame.display.set_caption("Evolution")
         self.FPS = FPS
         self.clock = pygame.time.Clock()
+        self.paused = False
 
     def run(self):
         for i in range(5):
-            cell = Cell(randint(0, 800), randint(0, 400))
+            cell = Cell(randint(0, WIDTH//4), randint(0, HEIGHT), 0)
             all_sprites.add(cell)
             cell_sprites.add(cell)
 
-        while True:
+        running = True
+        while running:
             self.clock.tick(self.FPS)
             self.window.fill((255, 255, 255))
+            pygame.draw.rect(self.window, (255, 206, 206), (WIDTH/4, 0, WIDTH/4, HEIGHT))
+            pygame.draw.rect(self.window, (238, 176, 176), (WIDTH / 2, 0, WIDTH / 4, HEIGHT))
+            pygame.draw.rect(self.window, (255, 146, 146), (3 * WIDTH / 4, 0, WIDTH / 4, HEIGHT))
             all_sprites.update()
             all_sprites.draw(self.window)
 
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    print(len(cell_sprites))
+                    # конечная численность клеток
+                    # print(len(cell_sprites))
+                    running = False
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.pause()
+
+    def pause(self):
+        self.paused = True
+        while self.paused:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    pygame.quit()
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_SPACE:
+                        self.paused = False
+        self.clock.tick(FPS)
 
 
 main = Main()
